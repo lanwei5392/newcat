8ea71bc0d3b2f8c08ae1c44eadd3021595491f60
lanwei5392 <lanweius@yahoo.com>
2016-11-22 00:24:35 -0800

    upload to github

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//set up model
+var	model = {
+
+	cats: [
+
+	{
+		name: 'Mimi',
+		count: 0,
+		image: 'img/cat-1.jpg'
+	},
+
+	{
+		name: 'Lily',
+		count: 0,
+		image: 'img/cat-2.jpg'
+	},
+
+	{
+		name: 'Sunny',
+		count: 0,
+		image: 'img/cat-3.jpg'
+	},
+
+	{
+		name: 'Jackie',
+		count: 0,
+		image: 'img/cat-4.jpg'
+	},
+
+	{
+		name: 'Yuki',
+		count: 0,
+		image: 'img/cat-5.jpg'
+	}
+	]
+};
